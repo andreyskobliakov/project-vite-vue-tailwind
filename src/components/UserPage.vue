6da9994a9d6d0a73fe117e1239caddc68c60437e
andreyskobliakov <a.skobliakov@gmail.com>
2023-08-04 13:58:07 +0300

    Добавил мой проект Vue с использованием Vite, Vue и Tailwind CSS

---
+++ src/components/UserPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>{{ user.name }}</h2>
+    <p>Email: {{ user.email }}</p>
+    <p>Phone: {{ user.phone }}</p>
+    <!-- Добавьте другую информацию о пользователе -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  created() {
+    this.fetchUserData();
+  },
+  methods: {
+    fetchUserData() {
+      const userId = this.$route.params.id;
+      axios
+        .get(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((response) => {
+          this.user = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
